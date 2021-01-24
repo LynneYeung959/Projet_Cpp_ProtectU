69ac873c54b1d83ae1a7f87d144c0b1375c786e8
LynneYeung959 <73192171+LynneYeung959@users.noreply.github.com>
2021-01-24 10:33:34 +0100

    ver2222

--- ProjetCpp/Main.cc
+++ ProjetCpp/Main.cc
@@ -5,14 +5,8 @@
 #include <string>
 #include <list>
 #include <map>
-//#include "Grave.hh"
-//#include "leger.hh"
-//#include "Docteur.hh"
+
 #include "personnage.hh"
-//#include "Employe.hh"
-//#include "Enfant.hh"
-//#include "Etudiant.hh"
-//#include "Infirmiere.hh"
 //#include "masque.hh"
 //#include "gel.hh"
 //#include "gant.hh"
@@ -25,22 +19,15 @@ using namespace std;
 namespace Caractere{
 typedef enum Caractere{Doctor, Infirmiere, Etudiante, Employe, Enfant} Caractere;
 };
-
-
-
 ostream& operator<<(ostream& out, Caractere::Caractere c);
 */
-/*
-list<Personnage> create(){
-	list<Personnage> les_perso;
-	Docteur d("Docteur Alex");
-	les_perso.push_back(d);
-	return les_perso;
-};
-*/
-typedef pair<string,int> ROLE;
-typedef pair<int, string> QUESTION;
 
+typedef pair<string,int> ROLE;
+
+/*--------------------------------------------------
+	Fonctin à récupérer les rôles exists dans le ficher role.txt
+
+--------------------------------------------------*/
 vector<ROLE> listRole(){
 	vector<ROLE> role;
 	ifstream file("role.txt");
@@ -63,15 +50,22 @@ vector<ROLE> listRole(){
 	}
 	return role;
 };
-/*
+
+/*--------------------------------------------------
 	Fonction du début de jeu, elle affiche la liste des personnages à choisir
 	void start(vector<ROLE> r)
 		- vector<ROLE> r : la liste des personnages à choisir
 							cette liste a été enovyé par la fonction vector<ROLE> listRole()
-*/
+--------------------------------------------------*/
 void start(vector<ROLE> r){
+	cout << "\n\tRègle du jeu:"<<endl;
+	cout << "\n\tVous choisissez une personnage, puis répondez une série de questions.";
+	cout << " Selon votre choix, le point de l'immunité et le point de protégé de votre personnage sera augmenter ou diminuer.";
+	cout << " Lorsque le point de protégé est inférieur à XX, le point de l'immunité va diminuer.";
+	cout << " Lorsque le point de l'immunité de votre de personnage est inférieur à YY, votre personnage sera être contaminé.";
+	cout << " Le jeu sera fini.\n\n";
 	cout << "\n\t--------GAME START--------\n\n";
-    cout << "Saisir le caractére préféré dans la liste suivante : \n\n";
+    cout << " Saisir le caractére préféré dans la liste suivante : \n\n";
     cout << "--------------------------------------\n";
     cout <<"\tMetier    Point de l'immunité"  << "\n"<< endl;
     for(auto it= r.begin(); it!=r.end(); ++it)
@@ -81,14 +75,15 @@ void start(vector<ROLE> r){
     cout<< "---------------------------------------\nJoueur :"<<endl;
 
 };
-/*
+
+/*--------------------------------------------------
 	Fonction renvoye le point de l'immunité de personnage choisi par joueur
 
 	int parametreRole(string choix, vector<ROLE> lRole)
 		- string choix: le nom de la personnage choisi du joueur,
 		- vecotr<ROLE> lRole: la liste de personnages à choisir
 			ce paramètre a été récupéré dans la fonction : vector<ROLE> listRole()
-*/
+----------------------------------------------------*/
 int parametreRole(string choix, vector<ROLE> lRole)
 {
 	for(auto it=lRole.begin(); it!=lRole.end(); ++it)
@@ -100,39 +95,21 @@ int parametreRole(string choix, vector<ROLE> lRole)
 	}
 	return 0;
 }
-/*
+
+/*--------------------------------------------------
 	Fonction à créer la personnage choisi par joueur
 
 	Personnage roleCreer(string choix, int pointImmu)
 		- strng choix: le nom de la personnage choisi par joueur
 		- int pointImmu: le point de l'immunité de la personnage choisi
-*/
+--------------------------------------------------*/
 Personnage* roleCreer(string choix, int pointImmu)
 {
 	return new Personnage(choix,pointImmu);
 };
-/*
-vector<QUESTION> qDocteur()
-{
-	ifstream file("docteur.txt");
-	vector<QUESTION > quesDoctor;
-}
-*/
-/*vector<QUESTION> */void qDocteur()
-{
-	ifstream file("docteur.txt");
-	vector<QUESTION> quesDoctor;
-	string line;
-	while(getline(file,line))
-	{
-		cout<< "BRAVO" << endl;
-	}
-
-}
 
 int main()
 {	
-
 	// Creer un HITOIRE pour afficher les l'ordre de joueur
 	// map? or vector?
 	//string nameJoueur;
@@ -143,31 +120,66 @@ int main()
 	//int go = 0; //indiquer si le rôle est bien créé
 	while(1)
 	{
-	    
 	    cin >> reponse;
 	    if(!cin)	//eviter l'erreur si la réponse n'est pas du type string
 	    {
 	    	cin.clear();
 	    	cin.sync();
-	    	cout << "\tSYSTEME: Je ne connais pas votre choix. Veuillez entrer à nouveau \nJoueur :"<<endl;
+	    	cout << "\t----SYSTEME: Je ne connais pas votre choix. Veuillez entrer à nouveau \nJoueur :"<<endl;
 	    	continue;
 	    }
 	    if (parametreRole(reponse,r)==0)
-	    {	cout << "\tSYSTEME: Je ne connais pas votre choix. Veuillez entrer à nouveau \nJoueur :"<<endl;
+	    {	cout << "\t----SYSTEME: Je ne connais pas votre choix. Veuillez entrer à nouveau \nJoueur :"<<endl;
 	    	continue;	
 	    }
-	    perso=roleCreer(reponse,parametreRole(reponse,r));
-	    cout << "\tSYSTEME: Rôle est bien créé !\n" <<endl;
+	    perso = new Personnage(reponse,parametreRole(reponse,r));
+	    //perso=roleCreer(reponse,parametreRole(reponse,r));
+	    cout << "\n\t\tSYSTEME: Rôle est bien créé !\n" <<endl;
 	    break;
 	}
+	cout << "\t\tVous allez répondre les questions suivantes. Répondre les questions avec 1 ou 2 ou 3 séparer avec l'espace"<<endl;
+	string numQuestion;
+	int cpt=1;
+	map<QUESTION,list<CHOIX>> script;
+	int flag;
+	while(1)
+	{	
+			numQuestion="Q"+to_string(cpt);
+			script = perso->lireScript(numQuestion);
+			if(script.empty())	// s'il n'y a pas de question à répondre, le jeu est fini
+				break;
+			perso->showScript(script); // afficher la question
+			cout << "\t----SYSTEME: Entrer votre choix"<<endl;
+			while(1)
+			{
+				cin >> flag;
+				if(!cin)	//eviter l'erreur si la réponse n'est pas du type int
+			    {
+			    	cin.clear();
+			    	cin.sync(); // effacer le flux d'entrée
+			    	cout << "\t----SYSTEME: Je ne connais pas votre choix. Veuillez entrer à nouveau \nJoueur :"<<endl;
+			    	continue;
+			    }
+			    if(flag!=1 && flag!=2 && flag!=3 && flag!=4)
+			    {
+			    	cout << "\t----SYSTEME: Saisir le chiffre entre 1 et 4 \nJoueur :"<<endl;
+			    	continue;
+			    }	
+			    else
+			    	break;
+			}
 
-	 if(reponse == "Docteur")
-	    	{
-	    		qDocteur();
-	    	/*else if(reponse == "Vieillard")*/
-	    }
-	perso->toString();
-	
+			switch(flag)
+			{
+				case 1:break;
+				default: break;
+			}
+		    	numQuestion.clear();
+				cpt++;		
+	}
+
+	//perso->showScript(script);
+	//perso->toString();
 };
 
 
